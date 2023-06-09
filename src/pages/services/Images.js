2f075a0400a3179b063441d3e53b7acbd4a64a14
rezanotalive <hai.rezaprasetya@gmail.com>
2023-06-09 19:43:00 +0700

    delete intersection

--- src/pages/services/Images.js
+++ src/pages/services/Images.js
@@ -1,71 +1,12 @@
-import { gsap } from "gsap"
 import Image from "next/image"
-import { useEffect, useRef, useState } from "react"
 import Masonry from "react-masonry-css"
 
 export default function Images({ images }) {
-  const usersDomNodes = useRef([])
   const columnsObj = {
     default: 4,
     1100: 3,
     700: 2,
     500: 1,
-  }
-
-  const observer = useRef(null)
-  const targetsRef = useRef([])
-
-  useEffect(() => {
-    const options = {
-      root: null, // The root element to use as the viewport for checking visibility
-      rootMargin: "0px", // Margin around the root element
-      threshold: 1, // The percentage of the target element's visibility needed to trigger the callback
-    }
-    observer.current = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          animateElement(entry.target)
-        } else {
-          noanimateElement(entry.target)
-        }
-      })
-    }, options)
-
-    targetsRef.current.forEach((target) => {
-      if (target) {
-        observer.current.observe(target)
-      }
-    })
-
-    return () => {
-      if (observer.current) {
-        observer.current.disconnect()
-      }
-    }
-  }, [])
-
-  const addTargetRef = (target) => {
-    if (target && !targetsRef.current.includes(target)) {
-      targetsRef.current.push(target)
-    }
-  }
-
-  const animateElement = (element) => {
-    gsap.to(element, {
-      duration: 0.3,
-      stagger: 0.3,
-      y: 0,
-      ease: "power2.out",
-      opacity: 1,
-    })
-  }
-  const noanimateElement = (element) => {
-    gsap.to(element, {
-      duration: 0.3,
-      stagger: 0.5,
-      y: 0,
-      opacity: 0,
-    })
   }
 
   return (
@@ -76,10 +17,9 @@ export default function Images({ images }) {
         className='-ml-5 flex w-auto '
         columnClassName='pl-5 bg-clip-padding'
       >
-        {images.map(({ id, public_id }) => (
+        {images?.map(({ id, public_id }) => (
           <div
             key={id}
-            ref={addTargetRef}
             className='  mb-5 h-auto max-w-full break-before-avoid will-change-auto'
           >
             <Image
