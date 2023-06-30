6219d1cea021427e0b6357e56321fd6518b7c56b
rezanotalive <hai.rezaprasetya@gmail.com>
2023-06-30 20:30:05 +0700

    add mobileNav

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,29 +1,45 @@
+import { useState } from "react"
+import MobileMenu from "./MobileMenu"
+
 export default function Navbar() {
+  const [openMobile, setOpenMobile] = useState(false)
+
+  const handleOpen = () => {
+    setOpenMobile((prev) => !prev)
+  }
+
   return (
-    <div className='sticky inset-0 z-10 h-16 w-full border-b-[1.5px] border-black bg-white'>
-      <div className='mx-auto flex h-full w-full max-w-6xl flex-row items-center justify-between p-5'>
-        <div className=''>
-          <a href='' className='text-base font-bold'>
-            Rezanotalive
-          </a>
-        </div>
-        <div className='hidden gap-5 sm:flex'>
-          <a href='' className='text-base font-semibold'>
-            link 1
-          </a>
-          <a href='' className='text-base font-semibold'>
-            link 2
-          </a>
-          <a href='' className='text-base font-semibold'>
-            link 3
-          </a>
-        </div>
-        <div className='flex gap-5 sm:hidden'>
-          <a href='' className='text-base font-semibold'>
-            Menu
-          </a>
+    <>
+      <div className='sticky inset-0 z-10 h-16 w-full border-b-[1.5px] border-black bg-white'>
+        <div className='mx-auto flex h-full w-full max-w-6xl flex-row items-center justify-between p-5'>
+          <div className=''>
+            <a href='' className='text-base font-bold'>
+              Rezanotalive
+            </a>
+          </div>
+          <div className='hidden gap-5 sm:flex'>
+            <a href='' className='text-base font-semibold'>
+              link 1
+            </a>
+            <a href='' className='text-base font-semibold'>
+              link 2
+            </a>
+            <a href='' className='text-base font-semibold'>
+              link 3
+            </a>
+          </div>
+          <div className='flex gap-5 sm:hidden'>
+            <button
+              onClick={handleOpen}
+              href=''
+              className='text-base font-semibold'
+            >
+              Menu
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <MobileMenu open={openMobile} onClose={handleOpen} />
+    </>
   )
 }
